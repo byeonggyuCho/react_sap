4ed1d564410123c0521989bbaaee55a0cb3b4708
긍정왕 <kin7729@naver.com>
2019-06-10 00:48:25 +0900

    DOCS : add comments

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 /**
- *  라우트 안에 다른 라우트를 증의하는 바업ㅂ
+ *  라우트 안에 다른 라우트를 정의하는 방법
+ * 
+ * 라우트로 사용된 컴포넌트가 전달받는 props
+ *  # 1.locatoin
+ *  # 2.match
+ *  # 3.history
  * 
  */
 const Post = ({match}) => {
